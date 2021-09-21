3366c369a67ff4129413a55be7eaff6b762d2792
rileydevine <824055@mystma.org>
2021-09-21 09:07:25 -0500

    resubmitted

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def topFiveMovies(a,b,c,d,e):
     return (a + b + c + d + e)
 
 #print topfivemovies
-print(topFiveMovies("Pacific Rim", ", Godzilla King of The Monsters", ", Godzilla vs Kong", ", #Alive", ", SnowPiercer"))
+print(topFiveMovies("Pacific Rim", " Godzilla King of The Monsters", " Godzilla vs Kong", " #Alive", " SnowPiercer"))
 
