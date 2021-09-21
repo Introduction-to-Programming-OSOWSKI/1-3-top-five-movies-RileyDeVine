2c60ec1382192b0b9d65a385489bfc369e972575
rileydevine <824055@mystma.org>
2021-09-21 09:15:19 -0500

    resubmitted

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
 
 #define topfivemovies
-def topFiveMovies(a,b,c,d,e):
+def topFiveMovies( a, b, c, d, e):
 
     #return five movies
     return (a + b + c + d + e)
 
 #print topfivemovies
-print(topFiveMovies(" Shrek", " Shrek 2", " Shrek 3", " Shrek 4", " Shrek 5"))
+print(topFiveMovies("Shrek ", "Shrek 2 ", "Shrek 3 ", "Shrek 4 ", "Shrek 5 "))
 
