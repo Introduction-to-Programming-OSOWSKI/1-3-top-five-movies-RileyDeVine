59beaee4767d1edc06fc488006e9918a8f2b14e1
rileydevine <824055@mystma.org>
2021-09-21 09:05:08 -0500

    submitted top five movies

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define topfivemovies
+def topFiveMovies(a,b,c,d,e):
+
+    #return five movies
+    return (a + b + c + d + e)
+
+#print topfivemovies
+print(topFiveMovies("Pacific Rim", ", Godzilla King of The Monsters", ", Godzilla vs Kong", ", #Alive", ", SnowPiercer"))
+
